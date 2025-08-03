52fc793d991d63c950a1e7502d0b5419613df5ca
slabwu <134193743+slabwu@users.noreply.github.com>
2025-08-03 10:26:13 +0800

    Style resume

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,13 @@ import Content from './Content.jsx'
 import './App.css'
 
 function App() {
-  const [details, setDetails] = useState({
-    name: "John Doe",
-    email: "johndoe@gmail.com",
-    phone: "88129302",
-    country: "SG",
-  });
-
   return (
     <>
       <header>
         <h1>Curriculum Vitae</h1>
         <button className='print' onClick={() => window.print()}>Print</button>
       </header>
-      <Content details={details} setDetails={setDetails}></Content>
+      <Content></Content>
     </>
   )
 }
--- src/resume.jsx
+++ src/resume.jsx
@@ -6,29 +6,29 @@ export default function Resume({ details, education, experience }) {
                 {details.email} - {details.phone} - {details.country}
             </p>
 
+            <h2 className='header'>Education</h2>
             <hr />
-            <h2>Education</h2>
             {education.map((school) => (
-                <>
-                <h3>{school.name}</h3>
-                <h4>{school.degree}</h4>
-                <p>
-                    {school.start} - {school.end}
-                </p>
-                </>
+                <div className='container'>
+                    <div className='title'>
+                        <h3>{school.name}</h3>
+                        <p>{school.start} - {school.end}</p>
+                    </div>
+                    <h4>{school.degree}</h4>
+                </div>
             ))}
-            
-            <hr />
         
+            <h2 className='header'>Experience</h2>
+            <hr />
             {experience.map((job) => (
-                <>
-                <h3>{job.name}</h3>
-                <h4>{job.position}</h4>
-                <p>{job.description}</p>
-                <p>
-                    {job.start} - {job.end}
-                </p>
-                </>
+                <div className='container'>
+                    <div className='title'>
+                        <h3>{job.name}</h3>
+                        <p>{job.start} - {job.end}</p>
+                    </div>
+                    <h4>{job.position}</h4>
+                    <p>{job.description}</p>
+                </div>
             ))}
 
         </section>
